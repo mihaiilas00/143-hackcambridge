71ebc4093a6136836689b3892dccfa6753516588
Nikita <nikita2212@hotmail.co.uk>
2020-01-19 00:41:06 +0000

    txt me l8r

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ var map = new atlas.Map('myMap', {
   language: 'en-UK',
   authOptions: {
     authType: 'subscriptionKey',
-    subscriptionKey: 'Q7QiCa3REAUKcQDfoXaOeciRvN0THdwzmhNx0t8KbWo'
+    subscriptionKey: 'Q7QiCa3REAUKcQDfoXaOeciRvN0THdwzmhNx0t8KbWo',
+    clientID: "879384a8-948f-40b1-a44b-460e74def766",
+    getToken: function (resolve, reject, map) {
+      fetch(url).then(function (response) {
+        return response.text();
+      }).then(function (token) {
+        resolve(token);
+      });
+    }
   }
 });
 map.events.add('ready', function () {
@@ -21,36 +29,17 @@ map.events.add('ready', function () {
   map.getCanvasContainer().style.cursor = "pointer";
   /* Create a popup */
   var popup = new atlas.Popup();
-
   /* Upon a mouse click, open a popup at the clicked location and render in the popup the address of the clicked location*/
   map.events.add("click", function (e) {
-    //send a request to Azure Maps reverse address search API */
-    var url = "https://atlas.microsoft.com/search/address/reverse/json?";
-    url += "&api-version=1.0";
-    url += "&query=" + e.position[1] + "," + e.position[0];
-
-    /*Process request*/
-    fetch(url, {
-      headers: {
-        "Authorization": "Bearer " + map.authentication.getToken(),
-        "x-ms-client-id": "985e5872-eb9f-448a-a249-728d852360a5"
-      }
-    }).then(function (response) {
-      return response.json();
-    }).then(function (res) {
-      var popupContent = document.createElement("div");
-      popupContent.classList.add("popup-content");
-      var address = res["addresses"];
-      popupContent.innerHTML =
-        address.length !== 0
-          ? address[0]["address"]["freeformAddress"]
-          : "No address for that location!";
-      popup.setOptions({
-        position: e.position,
-        content: popupContent
-      });
-      // render the popup on the map 
-      popup.open(map);
+    var popupContent = document.createElement("div");
+    popupContent.classList.add("popup-content");
+    popupContent.innerHTML =
+      e.position
+    popup.setOptions({
+      position: e.position,
+      content: popupContent
     });
+    // render the popup on the map 
+    popup.open(map);
   });
 });
