0d9299c0e7b051ff4b0e85667dfe278203e5926f
Stefi <sp928@cam.ac.uk>
2020-01-19 00:47:41 +0000

    Added Shortest Path algorithm to the best region

---
+++ back_end/src/Edge.java
@@ -0,0 +1,11 @@
+
+public class Edge{
+    public Bin bin;
+    public double cost;
+
+    public Edge(Bin bin , double cost){
+        this.bin = bin;
+        this.cost = cost;
+    }
+}
+
--- back_end/src/Location.java
+++ back_end/src/Location.java
@@ -1,4 +1,13 @@
+import java.util.Random;
+
 public class Location {
     public double x;
     public double y;
+    public Location(double xmin, double xmax, double ymin, double ymax){
+        double random = new Random().nextDouble();
+        x = xmin + (random * (xmax - xmin));
+
+        random = new Random().nextDouble();
+        y = ymin + (random * (ymax - ymin));
+    }
 }
